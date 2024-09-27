e1b58e4e29a5594bbfb0c2a983d3c0dec79994b2
Hoang Tran <trhoang2003@gmail.com>
2024-09-27 14:51:32 -0700

    nn

--- train.py
+++ train.py
@@ -8,20 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
-env = gym.make('CarRacing-v2', continuous=False)
-print("Observation space: ", env.observation_space)
-print("Action space: ", env.action_space)
-
-s, info = env.reset()
-print(s.shape)
-
-plt.figure(figsize=(5, 5))
-plt.imshow(s)
-plt.axis('off')
-plt.show()
-
-
 import cv2
 
 def preprocess(img):
@@ -30,3 +16,72 @@ def preprocess(img):
     
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) / 255.0
     return img
+
+class ImageEnv(gym.Wrapper):
+    def __init__(
+        self,
+        env,
+        skip_frames=4,
+        stack_frames=4,
+        initial_no_op=50,
+        **kwargs
+    ):
+        super(ImageEnv, self).__init__(env, **kwargs)
+        self.initial_no_op = initial_no_op
+        self.skip_frames = skip_frames
+        self.stack_frames = stack_frames
+    
+    def reset(self):
+        # Reset the original environment.
+        s, info = self.env.reset()
+
+        # Do nothing for the next `self.initial_no_op` steps
+        for i in range(self.initial_no_op):
+            s, r, terminated, truncated, info = self.env.step(0)
+        
+        # Convert a frame to 84 X 84 gray scale one
+        s = preprocess(s)
+
+        # The initial observation is simply a copy of the frame `s`
+        self.stacked_state = np.tile(s, (self.stack_frames, 1, 1))  # [4, 84, 84]
+        return self.stacked_state, info
+    
+    def step(self, action):
+        # We take an action for self.skip_frames steps
+        reward = 0
+        for _ in range(self.skip_frames):
+            s, r, terminated, truncated, info = self.env.step(action)
+            reward += r
+            if terminated or truncated:
+                break
+
+        # Convert a frame to 84 X 84 gray scale one
+        s = preprocess(s)
+
+        # Push the current frame `s` at the end of self.stacked_state
+        self.stacked_state = np.concatenate((self.stacked_state[1:], s[np.newaxis]), axis=0)
+
+        return self.stacked_state, reward, terminated, truncated, info
+
+
+env = gym.make('CarRacing-v2', continuous=False)
+env = ImageEnv(env)
+
+
+class CNNActionValue(nn.Module):
+    def __init__(self, state_dim, action_dim, activation=F.relu):
+        super(CNNActionValue, self).__init__()
+        self.conv1 = nn.Conv2d(state_dim, 16, kernel_size=8, stride=4)  # [N, 4, 84, 84] -> [N, 16, 20, 20]
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)  # [N, 16, 20, 20] -> [N, 32, 9, 9]
+        self.in_features = 32 * 9 * 9
+        self.fc1 = nn.Linear(self.in_features, 256)
+        self.fc2 = nn.Linear(256, action_dim)
+        self.activation = activation
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view((-1, self.in_features))
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
